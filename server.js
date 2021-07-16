2dc90dc3ebc34dd0213e9789dad31e2b343d441d
Ian Fullmer <ianfullmer@yahoo.com>
2021-07-16 06:38:42 -0500

    preserving intial rough work locally(not pushing to git hub)

--- server.js
+++ server.js
@@ -1,17 +1,116 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser') // middleware
 const cors = require('cors')
 require('dotenv').config()
+// const mongoose = require('mongoose');
+let mongoose;
+try {
+  mongoose = require("mongoose");
+} catch (e) {
+  console.log(e);
+}
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.use(cors())
+//create exerciseSchema
+const { Schema } = mongoose;
+
+const exerciseSchema = new Schema ({
+  description: String,
+  duration: Number,
+  date: String
+})
+
+//create personSchema
+
+const personSchema = new Schema({
+  username: String,
+  log: [exerciseSchema]
+});
+
+//create Session model
+
+const Session = mongoose.model('Session', exerciseSchema);
+//create Person model
+const Person = mongoose.model('Person', personSchema);
+
+//bodyParser to recognize form input from index. HTML 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// You can POST to /api/users with form data username to create a new user. 
+// The returned response will be an object with username and _id properties.
+app.post('/api/users', (req, res) => {
+  const newPerson = new Person({ username: req.body.username });
+  newPerson.save((err, data) => {
+    res.json({ "username": data.username, "_id": data.id })
+  });
+  // res.json({"response": req.body.username});
+});
+
+// You can make a GET request to /api/users to get an array of all users. 
+// Each element in the array is an object containing a user's username and _id.
+
+app.get('/api/users', (req, res) => {
+  Person.find({}, '_id, username').exec(function (err, users) {
+    console.log('users : ', users);
+    // console.log('err', err);
+    res.json(users);
+  });
+});
+
+// You can POST to /api/users/:_id/exercises with form data description, duration, 
+// and optionally date. If no date is supplied, the current date will be used. 
+// The response returned will be the user object with the exercise fields added.
+
+app.post('/api/users/:_id/exercises', (req, res) => {
 
 
+  Person.findOneAndUpdate({_id: req.params._id}, {
+    // date: req.body.date,
+    duration: req.body.duration,
+    description: req.body.description
+  }, {new: true}, function (err, docs) {
+    if (err) {
+      console.log(err)
+    }
+    else {
+      console.log( docs);
+    }
+  }
+  )
 
+  Person.findById(req.params._id, function (err, person) {
+    res.json({
+      '_id': req.params._id, 'username': person.username, 'date': req.body.date,
+      'duration': parseFloat(req.body.duration), "description": req.body.description
+    })
+  });
+});
+
+// You can make a GET request to /api/users/:_id/logs to retrieve a full exercise log 
+// of any user. The returned response will be the user object 
+// with a log array of all the exercises added. Each log item has the description, 
+// duration, and date properties
+app.get('/api/users/:_id/logs', (req, res) => {
+  Person.findById(req.params._id, (err, docs) => {
+    console.log("Result : ", docs);
+    res.json(docs);
+  })
+
+});
+//Remove
+
+app.get('/api/users/:_id/86', (req, res) => {
+   Person.findByIdAndRemove(req.params._id, req.body, (err,data)=> {
+     !err ? console.log("Deleted!") : console.log(err);
+   })
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
